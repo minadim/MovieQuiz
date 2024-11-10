fc1a88d476257759f81bddc0abe358e695fb0a6a
Nadina <minadin.m@gmail.com>
2024-11-10 22:45:13 +0500

    Сохранение изменений

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -88,89 +88,89 @@ final class MovieQuizViewController: UIViewController {
     // приватный метод конвертации, который принимает моковый вопрос и возвращает вью модель для главного экрана
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let image = UIImage(named: model.image) ?? UIImage()
-        let questionText = model.text
-        let questionNumber = "\(currentQuestionIndex + 1) / \(questions.count)"
+            let questionText = model.text
+            let questionNumber = "\(currentQuestionIndex + 1) / \(questions.count)"
+            
+            return QuizStepViewModel(
+                image: image,
+                question: questionText,
+                questionNumber: questionNumber
+            )
+        }
         
-        return QuizStepViewModel(
-            image: image,
-            question: questionText,
-            questionNumber: questionNumber
-        )
-    }
-    
-    // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
-    
-    private func show(_ step: QuizStepViewModel) {
-        imageView.image = step.image
-        textLabel.text = step.question
-        counterLabel.text = step.questionNumber
-    }
-    // Метод для показа первого вопроса при загрузке
-    private func showFirstQuestion() {
-        let currentQuestion = questions[currentQuestionIndex]
-        let viewModel = convert(model: currentQuestion)
-        show(viewModel)
-    }
-    // Обработка ответа пользователя
-    private func handleAnswer(isYes: Bool) {
-    let isCorrect = (isYes == questions[currentQuestionIndex].correctAnswer)
-       
+        // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
         
-        showAnswerResult(isCorrect: isCorrect)
-    }
-    // Приватный метод, который меняет цвет рамки
-    private func showAnswerResult(isCorrect: Bool) {
-        if isCorrect {
+        private func show(_ step: QuizStepViewModel) {
+            imageView.image = step.image
+            textLabel.text = step.question
+            counterLabel.text = step.questionNumber
+        }
+        // Метод для показа первого вопроса при загрузке
+        private func showFirstQuestion() {
+            let currentQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: currentQuestion)
+            show(viewModel)
+        }
+        // Обработка ответа пользователя
+        private func handleAnswer(isYes: Bool) {
+            let isCorrect = (isYes == questions[currentQuestionIndex].correctAnswer)
+            
+            
+            showAnswerResult(isCorrect: isCorrect)
+        }
+        // Приватный метод, который меняет цвет рамки
+        private func showAnswerResult(isCorrect: Bool) {
+            if isCorrect {
                 correctAnswers += 1 // Увеличить счётчик правильных ответов
             }
-        // Настройка рамки в зависимости от правильности ответа
-               imageView.layer.masksToBounds = true
-               imageView.layer.borderWidth = 8
-imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.showNextQuestionOrResults()
+            // Настройка рамки в зависимости от правильности ответа
+            imageView.layer.masksToBounds = true
+            imageView.layer.borderWidth = 8
+            imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
             
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.showNextQuestionOrResults()
+                
+            }
         }
-    }
-    // приватный метод, который содержит логику перехода в один из сценариев
-
-    private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 { // 1
-            showResults()
-        } else { // 2
-            currentQuestionIndex += 1
-            // идём в состояние "Вопрос показан"
-            let nextQuestion = questions[currentQuestionIndex]
-            let viewModel = convert(model: nextQuestion)
-            
-            show(_: viewModel)
+        // приватный метод, который содержит логику перехода в один из сценариев
+        
+        private func showNextQuestionOrResults() {
+            if currentQuestionIndex == questions.count - 1 { // 1
+                showResults()
+            } else { // 2
+                currentQuestionIndex += 1
+                // идём в состояние "Вопрос показан"
+                let nextQuestion = questions[currentQuestionIndex]
+                let viewModel = convert(model: nextQuestion)
+                
+                show(_: viewModel)
+            }
         }
-    }
         private func showResults() {
             let resultViewModel = QuizResultsViewModel(
-                        title: "Результаты",
-                        text: "Вы правильно ответили на \(correctAnswers) из \(questions.count) вопросов!",
-                        buttonText: "Сыграть ещё раз"
-                    )
-                    show(quiz: resultViewModel)
-                }
-                
-                // приватный метод для показа результатов раунда квиза
-                // принимает вью модель QuizResultsViewModel и ничего не возвращает
-                private func show(quiz result: QuizResultsViewModel) {
-                    let alert = UIAlertController(
-                        title: result.title,
-                        message: result.text,
-                        preferredStyle: .alert
-                    )
+                title: "Результаты",
+                text: "Вы правильно ответили на \(correctAnswers) из \(questions.count) вопросов!",
+                buttonText: "Сыграть ещё раз"
+            )
+            show(quiz: resultViewModel)
+        }
+        
+        // приватный метод для показа результатов раунда квиза
+        // принимает вью модель QuizResultsViewModel и ничего не возвращает
+        private func show(quiz result: QuizResultsViewModel) {
+            let alert = UIAlertController(
+                title: result.title,
+                message: result.text,
+                preferredStyle: .alert
+            )
             
-let action = UIAlertAction(title: "Сыграть ещё раз", style: .default) { _ in
+            let action = UIAlertAction(title: "Сыграть ещё раз", style: .default) { _ in
                 // Сбрасываем игру и показываем первый вопрос
                 self.currentQuestionIndex = 0
                 self.correctAnswers = 0
-            let firstQuestion = self.questions[self.currentQuestionIndex]
-            let viewModel = self.convert(model: firstQuestion)
+                let firstQuestion = self.questions[self.currentQuestionIndex]
+                let viewModel = self.convert(model: firstQuestion)
                 self.show(viewModel)
             }
             
@@ -191,8 +191,9 @@ let action = UIAlertAction(title: "Сыграть ещё раз", style: .defaul
         let question: String
         let questionNumber: String
     }
-struct QuizResultsViewModel {
-    let title: String
-    let text: String
-    let buttonText: String
-}
+    struct QuizResultsViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+
