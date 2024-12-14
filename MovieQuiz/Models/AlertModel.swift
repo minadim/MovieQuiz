d90e5c85dfd93e137408490e6499c2b719ebff08
Nadina <minadin.m@gmail.com>
2024-12-14 15:38:44 +0500

    Внесла изменения

--- MovieQuiz/Models/AlertModel.swift
+++ MovieQuiz/Models/AlertModel.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct AlertModel {
-    var title: String          // Заголовок алерта
-    var message: String        // Сообщение в алерте
-    var buttonTitle: String    // Текст кнопки
-    var completion: () -> Void // Замыкание для действия по кнопке алерта
+    var title: String
+    var message: String
+    var buttonTitle: String
+    var completion: () -> Void
 }
--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -5,36 +5,28 @@
 //  Created by Nadin on 09.12.2024.
 //
 
-import Foundation
-
 import UIKit
 
 class AlertPresenter {
-    // Свойство для хранения экземпляра модели
     private let alertModel: AlertModel
     private weak var viewController: UIViewController?
-
-    // Инициализатор для инициализации модели и контроллера
+    
     init(alertModel: AlertModel, viewController: UIViewController) {
         self.alertModel = alertModel
         self.viewController = viewController
     }
-
-    // Метод для показа алерта
+    
     func showAlert() {
-        // Создаем UIAlertController с данными из модели
         let alertController = UIAlertController(
             title: alertModel.title,
             message: alertModel.message,
             preferredStyle: .alert
         )
-
-        // Добавляем кнопку и ее действие
+        
         alertController.addAction(UIAlertAction(title: alertModel.buttonTitle, style: .default) { _ in
             self.alertModel.completion()
         })
-
-        // Показываем алерт на контроллере
+        
         viewController?.present(alertController, animated: true, completion: nil)
     }
 }
